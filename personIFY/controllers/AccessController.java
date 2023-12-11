0ef76fd14ef923ac107be44cba6a3f57bf421c04
tareun <tjongcharoeun@gmail.com>
2023-12-10 23:18:49 -0800

    Update AccessController.java

--- personIFY/controllers/AccessController.java
+++ personIFY/controllers/AccessController.java
@@ -6,6 +6,7 @@ import com.example.demo.GetArtist;
 import com.example.demo.mysql.RecommendedArtist;
 import com.example.demo.mysql.RecommendedArtistRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,6 @@ public class AccessController
     @PostConstruct
     public void clearTableOnStartup() {
         // This method will be called after the application context is initialized.
-        // You can perform any initialization logic here.
         System.out.println("Clearing the table on startup...");
 
         recommendedArtistRepository.clearTable();
@@ -47,11 +47,13 @@ public class AccessController
             {
                 String artistID = GetArtist.getArtistID(accessToken,similarArtist);
                 String artistInfo = GetArtist.getArtistInfo(accessToken, artistID);
+                //String artistGenre = GetArtist.getArtistGenre(accessToken, artistID);
 
+                /*
                 RecommendedArtist recommendedArtist = new RecommendedArtist();
                 recommendedArtist.setArtistName(similarArtist);
                 recommendedArtistRepository.save(recommendedArtist);
-
+                */
                 return artistInfo;
             }
             else
@@ -64,5 +66,35 @@ public class AccessController
             return "Failed to get token";
         }
         //return Credentials.getAccessToken();
+    }
+
+    @Transactional
+    @PostMapping("/api/store-artist")
+    public String storeArtist(@RequestParam String artistName)
+    {
+
+        // Split the received data into artist name and genres
+        String[] parts = artistName.split("\nGenres: ");
+
+        if (parts.length == 2) {
+            String artName = parts[0];
+            String genres = parts[1];
+
+            // Check if the artist already exists in the database
+            if (recommendedArtistRepository.existsByArtistName(artName)) {
+                return "Artist already exists in the database.";
+            }
+
+            // Save the artist to the database
+            RecommendedArtist recommendedArtist = new RecommendedArtist();
+            recommendedArtist.setArtistName(artName);
+            recommendedArtist.setGenres(genres);
+            recommendedArtistRepository.save(recommendedArtist);
+
+            return "Artist stored in the database.";
+        }else
+        {
+            return "invalid data format";
+        }
     }
 }
