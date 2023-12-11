b401590e75156be029422686c7611d2a764ca31d
tareun <tjongcharoeun@gmail.com>
2023-12-10 21:47:53 -0800

    Update AccessController.java

--- cs370project/demo/src/main/java/com/example/demo/controllers/AccessController.java
+++ cs370project/demo/src/main/java/com/example/demo/controllers/AccessController.java
@@ -3,12 +3,31 @@ package com.example.demo.controllers;
 import com.example.demo.Credentials;
 import com.example.demo.GetArtist;
 
+import com.example.demo.mysql.RecommendedArtist;
+import com.example.demo.mysql.RecommendedArtistRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 public class AccessController
 {
+
+    @Autowired
+    private RecommendedArtistRepository recommendedArtistRepository;
+
+    @PostConstruct
+    public void clearTableOnStartup() {
+        // This method will be called after the application context is initialized.
+        // You can perform any initialization logic here.
+        System.out.println("Clearing the table on startup...");
+
+        recommendedArtistRepository.clearTable();
+        recommendedArtistRepository.resetAutoIncrement();
+
+    }
+
     @GetMapping("/api/retrieve-data")
     public String getData(@RequestParam String artistName)
     {
@@ -28,6 +47,11 @@ public class AccessController
             {
                 String artistID = GetArtist.getArtistID(accessToken,similarArtist);
                 String artistInfo = GetArtist.getArtistInfo(accessToken, artistID);
+
+                RecommendedArtist recommendedArtist = new RecommendedArtist();
+                recommendedArtist.setArtistName(similarArtist);
+                recommendedArtistRepository.save(recommendedArtist);
+
                 return artistInfo;
             }
             else
